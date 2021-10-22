c8ea8ecec1e44564728d3e61a0791e7374c86eab
Giulia Ioana <giulia@IPirgariu-NB.luxoft.com>
2021-10-22 20:29:01 +0300

    Add post

--- src/app.py
+++ src/app.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
-from flask import Flask, json
+from flask import Flask, json, request
+
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
-
+products = [{"id": 1, "name": "laptop"}, {"id": 1, "name": "headphones"}]
 api = Flask(__name__)
 
 @api.route('/companies', methods=['GET'])
 def get_companies():
   return json.dumps(companies)
+
+@api.route('/products', methods=['GET'])
+def get_produts():
+  return json.dumps(products)
+
+@api.route('/products', methods=['POST'])
+def post_products():
+  products.append(request.get_json(force=True))
+  return '', 204 
+  
+
+
 
 if __name__ == '__main__':
     api.run(host="0.0.0.0", debug=True)
